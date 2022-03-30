d91ddd52c9cd11aa18ed1f9e064b02adb95686f6
wifec <olga.alonina@gmail.com>
2022-03-29 21:28:35 -0400

    Setting up test case 001

---
+++ src/March29_day1/testCase001.java
@@ -0,0 +1,12 @@
+package March29_day1;
+
+public class testCase001 {
+    //*NOTE: This link is unique to you, please do not share with others to avoid cancellation.*
+    //
+    //We strongly suggest you add to your calendar of your choice using below link.
+    //Add to Calendar Add to Google Calendar   Add to Yahoo Calendar
+    //
+    //Please submit any questions to support@cybertekschool.com.
+    //
+    //Starting Date Time: Mar 28, 2022 07:00 PM Eastern Time (US and Canada)
+}
