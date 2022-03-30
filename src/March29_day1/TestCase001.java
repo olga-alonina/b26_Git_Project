bbcdca6050cb0cc153065d7f973c3735e2828f2c
wifec <olga.alonina@gmail.com>
2022-03-29 20:36:23 -0400

    created test case 001

---
+++ src/March29_day1/TestCase001.java
@@ -0,0 +1,11 @@
+package March29_day1;
+
+public class TestCase001 {
+    //Is Y a Vowel?
+    //Using the formal definition above, the letter Y in words like "hymn" and "shy" is also a vowel.
+    // However, in words like "beyond" and "yes," Y is a consonant because the breath is partly obstructed.
+    //
+    //So, is Y a vowel? Well, sometimes it is, and sometimes it isn't, which is why it is often called a
+    // semi-vowel. The argument for classifying Y as a consonant (which most do) is based on this:
+    // When Y is a vowel, it is really just an I. When it is a consonant, it is being itself.
+}
